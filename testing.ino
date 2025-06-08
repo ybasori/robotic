d09c82a912f3dbdd9759bff24be11c53737047a5
Yusuf <ybasori@gmail.com>
2025-06-09 05:44:19 +0700

    Update testing.ino

--- testing.ino
+++ testing.ino
@@ -12,16 +12,18 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVO_MAX  512  // ~2 ms pulse (180 degrees)
 
 
-uint8_t HIP_JOINT_RIGHT = 8;
-uint8_t THIGH_RIGHT = 9;
-uint8_t CALV_RIGHT = 10;
-uint8_t ANKLE_RIGHT = 11;
+uint8_t HIP_JOINT_LEFT = 8;
+uint8_t THIGH_LEFT = 9;
+uint8_t CALV_LEFT = 10;
+uint8_t ANKLE_LEFT = 11;
 
-uint8_t HIP_JOINT_LEFT = 12;
-uint8_t THIGH_LEFT = 13;
-uint8_t CALV_LEFT = 14;
-uint8_t ANKLE_LEFT = 15;
+uint8_t HIP_JOINT_RIGHT = 12;
+uint8_t THIGH_RIGHT = 13;
+uint8_t CALV_RIGHT = 14;
+uint8_t ANKLE_RIGHT = 15;
 
+
+bool oneTime = false;
 
 void setup() {
   Serial.begin(115200);
@@ -34,48 +36,126 @@ void setup() {
 
 void loop() {
 
+if(oneTime){
+  oneTime = false;
+  animateStandUp();
+}
   animateStand();
   delay(1000);
 
-  animateSquad();
-  delay(1000);
+  // animateSquad();
+  // delay(1000);
 
   // normalizeTo90Degrees();
   // delay(1000);
 
 
+
 }
 
 // NOT AVAILABLE ON 0 DEGREES
-// HIP_JOINT_RIGHT, THIGH_RIGHT, CALV_LEFT, ANKLE_RIGHT,  
+// HIP_JOINT_LEFT, THIGH_LEFT, CALV_RIGHT, ANKLE_LEFT,  
 
 
 // NOT AVAILABLE ON 180 DEGREES
-// HIP_JOINT_LEFT, THIGH_LEFT, CALV_RIGHT, ANKLE_LEFT,
+// HIP_JOINT_RIGHT, THIGH_RIGHT, CALV_LEFT, ANKLE_RIGHT,
+
+void animateStandUp(){
+  Serial.println("Stand Up");
+    setServoAngle(HIP_JOINT_RIGHT, 90);
+    setServoAngle(HIP_JOINT_LEFT, 90);
+
+    setServoAngle(THIGH_RIGHT, 90);
+    setServoAngle(THIGH_LEFT, 90);
+
+    setServoAngle(CALV_RIGHT, 90);
+    setServoAngle(CALV_LEFT, 90);
+
+    setServoAngle(ANKLE_RIGHT, 0);
+    setServoAngle(ANKLE_LEFT, 180);
+
+    
+  delay(1000);
+
+    setServoAngle(HIP_JOINT_RIGHT, 90);
+    setServoAngle(HIP_JOINT_LEFT, 90);
+
+    setServoAngle(THIGH_RIGHT, 0);
+    setServoAngle(THIGH_LEFT, 180);
+
+    setServoAngle(CALV_RIGHT, 180);
+    setServoAngle(CALV_LEFT, 0);
+
+    setServoAngle(ANKLE_RIGHT, 0);
+    setServoAngle(ANKLE_LEFT, 180);
+
+  delay(1000);
+
+  
+    setServoAngle(HIP_JOINT_RIGHT, 90);
+    setServoAngle(HIP_JOINT_LEFT, 90);
+
+    setServoAngle(THIGH_RIGHT, 0);
+    setServoAngle(THIGH_LEFT, 180);
+
+    setServoAngle(CALV_RIGHT, 180);
+    setServoAngle(CALV_LEFT, 0);
+
+    setServoAngle(ANKLE_RIGHT, 90);
+    setServoAngle(ANKLE_LEFT, 90);
+
+  delay(1000);
+  
+    setServoAngle(HIP_JOINT_RIGHT, 0);
+    setServoAngle(HIP_JOINT_LEFT, 180);
+
+    setServoAngle(THIGH_RIGHT, 0);
+    setServoAngle(THIGH_LEFT, 180);
+
+    setServoAngle(CALV_RIGHT, 180);
+    setServoAngle(CALV_LEFT, 0);
+
+    setServoAngle(ANKLE_RIGHT, 0);
+    setServoAngle(ANKLE_LEFT, 180);
+  delay(1000);
+  
+    setServoAngle(HIP_JOINT_RIGHT, 0);
+    setServoAngle(HIP_JOINT_LEFT, 180);
+
+    setServoAngle(THIGH_RIGHT, 90);
+    setServoAngle(THIGH_LEFT, 90);
+
+    setServoAngle(CALV_RIGHT, 90);
+    setServoAngle(CALV_LEFT, 90);
+
+    setServoAngle(ANKLE_RIGHT, 0);
+    setServoAngle(ANKLE_LEFT, 180);
+  delay(1000);
+}
 
 void animateSquad(){
 
   Serial.println("Squad");
   
-    setServoAngle(HIP_JOINT_RIGHT, 180);
-    setServoAngle(HIP_JOINT_LEFT, 0);
+    setServoAngle(HIP_JOINT_RIGHT, 0);
+    setServoAngle(HIP_JOINT_LEFT, 180);
   
-    setServoAngle(THIGH_RIGHT, 180);
-    setServoAngle(THIGH_LEFT, 00);
+    setServoAngle(THIGH_RIGHT, 0);
+    setServoAngle(THIGH_LEFT, 180);
 
-    setServoAngle(CALV_RIGHT, 0);
-    setServoAngle(CALV_LEFT, 180);
+    setServoAngle(CALV_RIGHT, 180);
+    setServoAngle(CALV_LEFT, 0);
 
-    setServoAngle(ANKLE_RIGHT, 180);
-    setServoAngle(ANKLE_LEFT, 0);
+    setServoAngle(ANKLE_RIGHT, 0);
+    setServoAngle(ANKLE_LEFT, 180);
   
 }
 
 void animateStand(){
   Serial.println("Stand");
   
-    setServoAngle(HIP_JOINT_RIGHT, 180);
-    setServoAngle(HIP_JOINT_LEFT, 0);
+    setServoAngle(HIP_JOINT_RIGHT, 0);
+    setServoAngle(HIP_JOINT_LEFT, 180);
   
     setServoAngle(THIGH_RIGHT, 90);
     setServoAngle(THIGH_LEFT, 90);
@@ -83,8 +163,8 @@ void animateStand(){
     setServoAngle(CALV_RIGHT, 90);
     setServoAngle(CALV_LEFT, 90);
 
-    setServoAngle(ANKLE_RIGHT, 180);
-    setServoAngle(ANKLE_LEFT, 0);
+    setServoAngle(ANKLE_RIGHT, 0);
+    setServoAngle(ANKLE_LEFT, 180);
   
 }
 
